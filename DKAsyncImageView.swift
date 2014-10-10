7d3c1f4fb5d8ccbbb4acb8836c2311601bf4b260
Dave <snpukwana@gmail.com>
2014-10-09 21:24:44 -0400

    1 Hour

--- DKAsyncImageView.swift
+++ DKAsyncImageView.swift
@@ -23,3 +23,97 @@
 //  SOFTWARE.
 
 import Cocoa
+
+class DKAsyncImageView: NSImageView, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
+    private var imageURLConnection: NSURLConnection?
+    private var imageDownloadData: NSMutableData?
+    private var errorImage: NSImage?
+    
+    private var spinningWheel: NSProgressIndicator?
+    private var trackingArea: NSTrackingArea?
+    
+    private var isLoadingImage: Bool = false
+    private var userDidCancel: Bool = false
+    private var didFailLoadingImage: Bool = false
+    
+    private var toolTipWhileLoading: String = ""
+    private var toolTipWhenFinished: String = ""
+    private var toolTipWhenFinishedWithError: String = ""
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func downloadImageFromURL(url: String) {
+        downloadImageFromURL(url, placeHolderImage: nil, errorImage: nil, usesSpinningWheel: false)
+    }
+    
+    func downloadImageFromURL(url: String, placeHolderImage: NSImage?) {
+        downloadImageFromURL(url, placeHolderImage: placeHolderImage, errorImage: nil, usesSpinningWheel: false)
+    }
+    
+    func downloadImageFromURL(url: String, placeHolderImage: NSImage?, errorImage: NSImage?) {
+        downloadImageFromURL(url, placeHolderImage: placeHolderImage, errorImage: errorImage, usesSpinningWheel: false)
+    }
+    
+    func downloadImageFromURL(url: String, placeHolderImage:NSImage?, errorImage:NSImage?, usesSpinningWheel: Bool) {
+        cancelDownload()
+        
+        isLoadingImage = true
+        didFailLoadingImage = false
+        userDidCancel = false
+        
+        image = placeHolderImage
+        self.errorImage = errorImage
+        imageDownloadData = NSMutableData()
+        
+        var URL = NSURL(string: url)
+        
+        if URL == nil {
+            isLoadingImage = false
+            NSLog("Error: malformed URL passed to downloadImageFromURL")
+            return
+        }
+            
+        var conn: NSURLConnection? = NSURLConnection(request: NSURLRequest(URL: URL!), delegate: self)
+        imageURLConnection = conn
+        
+        if usesSpinningWheel {
+            if self.frame.size.height >= 64 && self.frame.size.width >= 64 {
+                spinningWheel = NSProgressIndicator()
+                if let spinningWheel = spinningWheel {
+                    addSubview(spinningWheel)
+                    spinningWheel.displayedWhenStopped = false
+                    spinningWheel.frame = NSMakeRect(self.frame.size.width * 0.5 - 16, self.frame.size.height * 0.5 - 16, 32, 32)
+                    spinningWheel.controlSize = NSControlSize.RegularControlSize
+                    spinningWheel.startAnimation(self)
+                }
+                
+            } else if 
+        }
+    }
+    
+    func cancelDownload() {
+        userDidCancel = true
+        isLoadingImage = false
+        didFailLoadingImage = false
+        
+        deleteToolTips()
+        
+        spinningWheel?.stopAnimation(self)
+        spinningWheel?.removeFromSuperview()
+        
+        imageURLConnection?.cancel()
+        imageURLConnection = nil
+        imageDownloadData = nil
+        errorImage = nil
+        image = nil
+    }
+    
+    func deleteToolTips() {
+        toolTip = ""
+        toolTipWhileLoading = ""
+        toolTipWhenFinished = ""
+        toolTipWhenFinishedWithError = ""
+    }
+}
