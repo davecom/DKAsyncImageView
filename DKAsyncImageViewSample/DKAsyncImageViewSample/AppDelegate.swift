0eb182d806902f7395c5ea0323b7b55f3570f346
Dave <snpukwana@gmail.com>
2014-10-11 07:12:56 -0400

    Added Sample Project

---
+++ DKAsyncImageViewSample/DKAsyncImageViewSample/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DKAsyncImageViewSample
+//
+//  Created by David Kopec on 10/11/14.
+//  MIT Licensed
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+    @IBOutlet weak var imageView: DKAsyncImageView!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        imageView.downloadImageFromURL("http://www.google.com/images/logo.gif")
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+
+
+}
+
